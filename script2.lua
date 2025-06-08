ca4879be504ba559d4f20935d0b8048f21666e98
LXFS <152890725+lxfservice@users.noreply.github.com>
2025-06-08 14:05:38 +0200

    Update script2.lua

--- script2.lua
+++ script2.lua
@@ -2,8 +2,9 @@ local ESX = exports.es_extended:getSharedObject()
 
 ESX.RegisterServerCallback('lxfs_hud:getBankMoney', function(source, cb)
     local xPlayer = ESX.GetPlayerFromId(source)
+        if xPlayer then
     local bankMoney = 0
-
+    
     for _, account in pairs(xPlayer.getAccounts()) do
         if account.name == 'bank' then
             bankMoney = account.money
@@ -12,4 +13,7 @@ ESX.RegisterServerCallback('lxfs_hud:getBankMoney', function(source, cb)
     end
 
     cb(bankMoney)
+        else
+            print("Error while loading xPlayer")
+        end
 end)
